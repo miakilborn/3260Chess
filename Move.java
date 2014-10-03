a24c687a1d1ad3d74e74b44e12f21901cd0d0fe0
Timothy Engel <engeltj@gmail.com>
2014-10-03 16:47:56 -0400

    Move class created

--- Move.java
+++ Move.java
@@ -1,4 +1,27 @@
-public class AbleBaker {
+public class Move {
+	private boolean valid;
+	private final Piece piece;
+	private final Coordinate nextPosition;
+
+	private final Piece captured;
+
+	public Move(Piece piece, Coordinate nextPosition){
+		this.piece = piece;
+		this.nextPosition = nextPosition;
+	}
+
+	public void setPieceCaptured(Piece piece){
+		captured = piece;
+	}
+
+	public boolean isValid(){
+		return valid;
+	}
+
+	public Piece getPieceCaptured(){
+		return captured;
+	}
 
 
+	
 }
