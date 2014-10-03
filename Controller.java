d3f44e566860f0a47f781274c7dadf1d01ffd52f
Bill Vandenberk <bill@vandenberk.me>
2014-10-03 15:36:01 -0400

    Controller working with basic method calls

--- Controller.java
+++ Controller.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
+import java.lang.reflect.Method;
 
 public class Controller{
     public static final Boolean MASTER = true;
@@ -13,22 +14,22 @@ public class Controller{
     private BufferedReader socketIn;
 
     public Controller(String test){
-            ServerSocket serverPipe = null;
-            Socket clientPipe = new Socket();
+            ServerSocket masterPipe = null;
+            Socket slavePipe = new Socket();
             InetSocketAddress address = new InetSocketAddress("127.0.0.1",PORT);
 
             //Set up connection
             try{
-                serverPipe = new ServerSocket(address.getPort());
+                masterPipe = new ServerSocket(address.getPort());
                 mode = MASTER;
                 System.out.println("You are master, waiting for slave...");
-                clientPipe = serverPipe.accept();
+                slavePipe = masterPipe.accept();
             }
             catch(IOException e){
                 //Address already in use. Connect to it.
                 try{
                     System.out.println("You are slave, connecting to master...");
-                    clientPipe.connect(address);
+                    slavePipe.connect(address);
                     mode = SLAVE;
                 }
                 catch(IOException ioe){
@@ -43,8 +44,8 @@ public class Controller{
 
             //Set up stream
             try{
-                socketOut = new PrintWriter(clientPipe.getOutputStream(), true);
-                socketIn = new BufferedReader(new InputStreamReader(clientPipe.getInputStream()));
+                socketOut = new PrintWriter(slavePipe.getOutputStream(), true);
+                socketIn = new BufferedReader(new InputStreamReader(slavePipe.getInputStream()));
             }
             catch(IOException ioe){
                 System.err.println("Could not set up stream: "+ioe.toString());
@@ -56,8 +57,14 @@ public class Controller{
         socketOut.println(term);
     }
 
-    public String recieve() throws IOException{
-        return socketIn.readLine();
+    public String read(){
+        try{
+            return socketIn.readLine();
+        }
+        catch(IOException e){
+            System.err.println("Got nothing!"+e.toString());
+            return "";
+        }
     }
 
     public Boolean isMaster(){
@@ -68,31 +75,47 @@ public class Controller{
         return !mode;
     }
 
-    public static void main(String []args){
+    public Boolean test(String example){
+        System.out.println("TEST: "+example);
+        return true;
+    }
+
+    public static void main(String []args) throws InterruptedException{
         Controller me = new Controller("");
-        Scanner input = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
 
         if(me.isMaster()){
-            me.send("Hello From Master");
+            me.send("RDY");
+            String get = me.read();
+            me.send("ACK:"+get);
+
+            String [] cmd = get.split(",");
+
             try{
-                System.out.println(me.recieve());
+                Method method = me.getClass().getMethod(cmd[0],cmd[1].getClass());
+                method.invoke(me,cmd[1]);
             }
-            catch(IOException e){
-                System.err.println("Nothing recieved from master: "+e.toString());
+            catch(Exception e){
+                System.err.println(e.toString());
             }
         }
         else if(me.isSlave()){
-            try{
-                System.out.println(me.recieve());
+            String got = me.read();
+            if(got.equals("RDY")){
+                String message = "test,from slave";
+                me.send(message);
+                if(me.read().equals("ACK:"+message)){
+                    System.out.println("Master ACK... data valid");
+                }
+                else{
+                    //Master got something else
+                }
             }
-            catch(IOException e){
-                System.err.println("Nothing recieved from master: "+e.toString());
-            }
-            me.send("Hello from slave!");
+
         }
         else{
             System.err.println("An error occurred");
         }
-        return;
+        System.exit(0);
     }
 }
