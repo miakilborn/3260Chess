5bd06769386583876ac9d264c102be34d0c7bc7b
Tim Engel <engeltj@gmail.com>
2014-10-03 16:05:53 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,19 +7,35 @@ public class Player {
     private final String colour;
     private int score;
     
+    /**
+     * Construct player with the players game name, and colour
+     * 
+     */
     public Player(String name, String colour){
         this.name = name;
         this.colour = colour;
     }
     
+    /**
+     * Get the players game name
+     * 
+     */
     public String getPlayerName(){
         return name;
     }
     
+    /**
+     * Get the players game colour
+     * 
+     */
     public String getColour(){
         return colour;
     }
     
+    /**
+     * Get the players current game score
+     * 
+     */
     public int getScore(){
         return score;
     }
