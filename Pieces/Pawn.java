4bb3a873a4dd9dbbe428e5b0a6bc0e1b10f24a88
Mia Kilborn <miakilborn@gmail.com>
2014-10-03 23:06:49 -0400

    Update Pawn.java
    Modified to use the methods implemented in class's super.

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -1,39 +1,16 @@
+import Coordinate;
+
 public class Pawn extends Piece {
-	private final String colour;
-	private Coordinate position;
-	public Pawn(){
+    public Pawn(){
+        //empty constructor
+    }
+    
+    public Pawn(String colour, Coordinate position){
+        super.setColour(colour);
+        super.setPosition(position);
+    }
 
-	}
-
-	public Pawn(String colour, Coordinate position){
-		
-	}
-
-	public void setPosition(Coordinate position){
-
-	}
-
-	public void setPosition(int x, int y){
-		this.position.setX(x);
-		this.position.setY(y);
-	}
-
-	public Coordinate getPosition(){
-		return position;
-	}
-
-	public String getColour(){
-		return colour;
-	}
-
-	/**
-	*  set this piece to be captured state, to uncapture
-	*  set its position once again
-	*/
-	public void capture(){
-		setPosition(-1, -1)
-	}
-
-
+    public void capture(){
+        super.setPosition(-1, -1);
+    }
 }
-
