605ae56cd0cabadf36bea754f7bafd02fdee2a8a
Bill Vandenberk <bill@vandenberk.me>
2014-10-04 14:23:55 -0400

    Move can be made from and translated into strings

--- Game/Move.java
+++ Game/Move.java
@@ -14,10 +14,42 @@ public class Move {
 	}
 
 	public Move(String s){
+		//EXAMPLE: M(Rook(C(0,0),black,false),C(0,1))
+		if(s.charAt(0)=='M'){
+			s = s.replaceAll("M\\(|\\)$","");
+			String p = s.substring(0,s.lastIndexOf("),")+1);
+			s = s.replace(p+",","");
+			switch(p.substring(0,p.indexOf("("))){
+				case "Rook":
+					this.piece = new Rook(p);
+					break;
+				case "Knight":
+					this.piece = new Knight(p);
+					break;
+				case "Bishop":
+					this.piece = new Bishop(p);
+					break;
+				case "King":
+					this.piece = new King(p);
+					break;
+				case "Queen":
+					this.piece = new Queen(p);
+					break;
+				case "Pawn":
+					this.piece = new Pawn(p);
+					break;
+				default:
+					this.piece = null;
+			}
+			nextPosition = new Coordinate(s);
+		}
+		else{
+			piece = null;
+			nextPosition = null;
+		}
+
 		//Need a basic string constructor... this will rely on a coordinate
 		//toString and a coordinate string constructor as well.
-		piece = null;
-		nextPosition = null;
 
 	}
 
@@ -54,7 +86,6 @@ public class Move {
 
 	public String toString(){
 		//Should return a string that you can use in the constructor to rebuild the object.
-		return null;
+		return ("M("+piece.toString()+","+nextPosition.toString()+")");
 	}
-
 }
