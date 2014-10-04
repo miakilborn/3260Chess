bb09c750759ad08dad949c023b0d36a687283189
Mia Kilborn <miakilborn@gmail.com>
2014-10-03 23:21:24 -0400

    Update Coordinate.java
    Updated, nothing really changed.

--- Coordinate.java
+++ Coordinate.java
@@ -1,25 +1,25 @@
-public class Coordinate {
-	private int x;
-	private int y;
-
-	public Coordinate(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
-
-	public void setX(int x){
-		return x;
-	}
-
-	public void setY(int y){
-		return y;
-	}
-
-	public int getX(){
-		return x;
-	}
-
-	public int getY(){
-		return y;
-	}
+public class Coordinate{
+    private int x;
+    private int y;
+    
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
 }
