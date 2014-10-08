441792f11bcbe4880cb5c08cf79c31037486ec0e
Mia Kilborn <miakilborn@gmail.com>
2014-10-08 10:43:01 -0400

    Changed location of (1,1)

--- Game/Standard8x8Board.java
+++ Game/Standard8x8Board.java
@@ -48,7 +48,7 @@ public class Standard8x8Board implements IBoard {
         int x, y;
         String strBoard = "";
         char[] numsAcross = {' ', ' ', '1', ' ', '2', ' ', '3', ' ', '4', ' ', '5', ' ', '6', ' ', '7', ' ', '8', ' ', ' '};
-        char[] numsDown = {' ', '1', '2', '3', '4', '5', '6', '7', '8', ' '};
+        char[] numsDown = {' ', '8', '7', '6', '5', '4', '3', '2', '1', ' '};
         
         //initialize
         for(int i = 0; i < 10; i++){
