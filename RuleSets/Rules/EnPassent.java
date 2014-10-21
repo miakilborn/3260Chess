119d1aae0653fd5c073650bf8abe69d47b924296
Bill Vandenberk <bill@vandenberk.me>
2014-10-21 15:27:24 -0400

    fixed I think

--- RuleSets/Rules/EnPassent.java
+++ RuleSets/Rules/EnPassent.java
@@ -8,32 +8,26 @@ public class EnPassent implements IRule {
 
     }
     public Result checkAndMakeMove(IBoard board, IRuleSet rules, Move move){
-        Coordinate current = move.getCurrentPosition();
-        Coordinate newP = move.getNextPosition();
-        Piece myPiece = board.getPieceFromPosition(current);
         Move lastMove = rules.getLastMove();
+        if(lastMove==null){
+            return new Result(false);
+        }
+
+        Coordinate current = move.getCurrentPosition();
         Coordinate lastNextP = lastMove.getNextPosition();
-        Coordinate lastCurrent = lastMove.getCurrentPosition();
+        if(current==null || lastNextP==null){
+            return new Result(false);
+        }
+        Piece myPiece = board.getPieceFromPosition(current);
         Piece theirPiece = board.getPieceFromPosition(lastNextP);
-
-        if(!(myPiece instanceof Pawn) || !(theirPiece instanceof Pawn)){
+        if(myPiece==null || theirPiece==null){
             return new Result(false);
         }
 
-        //if last move was double jump pawn
-        if(Math.abs(lastNextP.getY()-lastCurrent.getY())!=2){
-            return new Result(false);
-        }
-
-        //if desired move is on an angle
-        if(Math.abs(current.getY()-newP.getY())==1 && Math.abs(current.getX()-newP.getX())==1){
-            //if desired move puts pawn behind other pawn
-            if((lastNextP.getY()==5 && newP.getY()==6 && lastNextP.getX()==newP.getX())
-            || (lastNextP.getY()==4 && newP.getY()==3 && lastNextP.getX()==newP.getX())){
-                theirPiece.capture();
-                myPiece.setPosition(newP);
-                return new Result(true);
-            }
+        if(checkMove(board,rules,move).isValid()){
+            theirPiece.capture();
+            myPiece.setPosition(move.getNextPosition());
+            return new Result(true);
         }
 
         return new Result(false);
