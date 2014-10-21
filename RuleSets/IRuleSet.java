05445eb6df1ee326a0c29031d59d26c9670563d6
Bill Vandenberk <bill@vandenberk.me>
2014-10-21 13:28:44 -0400

    started en passant, added getter for last move in ruleset

--- RuleSets/IRuleSet.java
+++ RuleSets/IRuleSet.java
@@ -4,18 +4,20 @@ import Pieces.*;
 import RuleSets.Rules.IRule;
 import java.util.*;
 public interface IRuleSet {
-        public boolean checkMovePawn(IBoard board, Move move);
-        public boolean checkMoveRook(IBoard board, Move move);
-        public boolean checkMoveKing(IBoard board, Move move);
-        public boolean checkMoveQueen(IBoard board, Move move);
-        public boolean checkMoveKnight(IBoard board, Move move);
-        public boolean checkMoveBishop(IBoard board, Move move);
-    
-    
+    public boolean checkMovePawn(IBoard board, Move move);
+    public boolean checkMoveRook(IBoard board, Move move);
+    public boolean checkMoveKing(IBoard board, Move move);
+    public boolean checkMoveQueen(IBoard board, Move move);
+    public boolean checkMoveKnight(IBoard board, Move move);
+    public boolean checkMoveBishop(IBoard board, Move move);
+
+
 	public void setupBoard(IBoard board);
 	public boolean hasWon(IBoard board, String colour);
-        
-        public Result makeMove(IBoard board, Move move, ArrayList<IRule> additionalRules);
+
+    public Move getLastMove();
+
+    public Result makeMove(IBoard board, Move move, ArrayList<IRule> additionalRules);
 	public Result checkMove(IBoard board, Move move, ArrayList<IRule> additionalRules);
 
 }
--- RuleSets/Rules/EnPassent.java
+++ RuleSets/Rules/EnPassent.java
@@ -8,8 +8,34 @@ public class EnPassent implements IRule {
 
     }
     public Result checkAndMakeMove(IBoard board, IRuleSet rules, Move move){
+        Coordinate current = move.getCurrentPosition();
+        Coordinate newP = move.getNextPosition();
+        Piece myPiece = board.getPieceFromPosition(current);
+        Move lastMove = rules.getLastMove();
+        Coordinate lastNextP = lastMove.getNextPosition();
+        Coordinate lastCurrent = lastMove.getCurrentPosition();
+        Piece theirPiece = board.getPieceFromPosition(lastNextP);
+
+        if(!(myPiece instanceof Pawn) || !(theirPiece instanceof Pawn)){
+            return new Result(false);
+        }
+
+        //if last move was double jump pawn
+        if(abs(lastNextP.getY()-lastCurrent.getY())!=2){
+            return new Result(false);
+        }
+
+        //if desired move is on an angle
+        if(abs(current.getY()-newP.getY())==1 && abs(current.getX()-newP.getX())==1){
+            //if desired move puts pawn behind other pawn
+            if(abs(current.getY()-lastNextP.getY())==1){
+                return new Result(false);
+            }
+        }
+
         return new Result(false);
     }
+
     public Result checkMove(IBoard board, IRuleSet rules, Move move){
         return new Result(false);
     }
