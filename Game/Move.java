347226fcc99dd9759a7817c30d0c05776c3bff3e
Timothy Engel <engeltj@gmail.com>
2014-10-21 14:58:44 -0400

    Fixed toString to also write in isGameOver flag, and constructor parses for that

--- Game/Move.java
+++ Game/Move.java
@@ -23,20 +23,28 @@ public class Move {
             this.nextPosition = new Coordinate(-1,-1);
         }
         else if(s.charAt(0) == 'M'){
+                System.err.println(s);
                 s = s.replaceAll("M\\(|\\)$","");
                 String c = s.substring(0,s.lastIndexOf("),")+1);
                 s = s.replace(c+",","");
 
                 String c1 = c.substring(0,c.lastIndexOf("),")+1);
                 c = c.replace(c1+",","");
-
+                
+                String[] booleans = s.split(",");
+                
                 currentPosition = new Coordinate(c1);
                 nextPosition = new Coordinate(c);
-                if(s.equals("true")){
+                if(booleans[0].equals("true")){
                         this.captured = new Coordinate(c);
                 }
+                
+                if (booleans[1].equals("true")){
+                    this.isDraw = true;
+                    System.err.println("This move is constructed with isDraw flag set TRUE");
+                }
         }
-        else{
+        else {
                 currentPosition = null;
                 nextPosition = null;
                 captured = null;
@@ -73,6 +81,6 @@ public class Move {
 
     public String toString(){
             //Should return a string that you can use in the constructor to rebuild the object.
-            return ("M("+currentPosition.toString()+","+nextPosition.toString()+","+(captured==null?"false":"true")+")");
+            return ("M("+currentPosition.toString()+","+nextPosition.toString()+","+(captured==null?"false":"true")+","+Boolean.toString(isDraw)+")");
     }
 }
