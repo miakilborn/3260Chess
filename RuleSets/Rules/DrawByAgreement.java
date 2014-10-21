cb7c9f080ca8473b922c23b1b98e4eca0dcba750
zkenshin <zack_licastro@hotmail.com>
2014-10-20 20:15:59 -0400

    Returned result from checkAndMakeMove

--- RuleSets/Rules/DrawByAgreement.java
+++ RuleSets/Rules/DrawByAgreement.java
@@ -5,24 +5,17 @@ import Pieces.*;
 import java.util.Scanner;
 public class DrawByAgreement implements IRule {
 
-    /* 
-        When draw by agreement is active,
-        player may enter "draw" instead of a move
-        to suggest a mutual draw. If the other user then
-        also enters draw, the game ends in a tie.
-        
-        Move will contain a isDraw(), if move has isDraw() true, return a result with Message draw, validness of true?
-    */
-
     public DrawByAgreement(){
 
     }
 
 
 
-
     public Result checkAndMakeMove(IBoard board, IRuleSet rules, Move move){
-        return new Result(false);
+        
+        Result result = checkMove(board, rules, move);
+    
+        return result;
     }
 
     public Result checkMove(IBoard board, IRuleSet rules, Move move){
